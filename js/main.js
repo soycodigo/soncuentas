3c03856758efd4095d957f39e91b5ad91559c178
Humberto <soycodigo@gmail.com>
2018-12-16 12:57:41 -0700

    Scroll en barra navegadora

--- js/main.js
+++ js/main.js
@@ -1,22 +1,25 @@
 $(document).ready(function () {
 
-    if ($(document).scrollTop() > 150) {
+    $(document).on("scroll", function () {
+        if ($(document).scrollTop() > 150) {
 
-        // $(".divrub").css({visibility: 'visible',width:'270px'});
-        $(".divrub").addClass('divmas');
-        $(".divrub").removeClass('divmenos');
-        $("#divabs").hide('fadeOut');
-        $("#divabs2").show();
-        $("#navtelmail").hide('fadeOut');
+            // $(".divrub").css({visibility: 'visible',width:'270px'});
+            $(".divrub").addClass('divmas');
+            $(".divrub").removeClass('divmenos');
+            $("#divabs").hide('fadeOut');
+            $("#divabs2").show();
+            $("#navtelmail").hide('fadeOut');
 
-    } else {
+        } else {
 
-        $("#divabs").show('fadeOut');
-        $("#navtelmail").show('fadeOut');
-        $("#divabs2").hide();
-        $(".divrub").addClass('divmenos');
-        $(".divrub").removeClass('divmas');
-        $(".divrub").addClass('divmenos');
+            $("#divabs").show('fadeOut');
+            $("#navtelmail").show('fadeOut');
+            $("#divabs2").hide();
+            $(".divrub").addClass('divmenos');
+            $(".divrub").removeClass('divmas');
+            $(".divrub").addClass('divmenos');
 
-    }
+        }
+    });
+
 });
