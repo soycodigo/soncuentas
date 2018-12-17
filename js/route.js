67e73d0b070d2ed340cb5fbbb50a7057e2cc2a59
Humberto <soycodigo@gmail.com>
2018-12-16 20:50:46 -0700

    Agregada vista de educacion y parte de la alfombra

--- js/route.js
+++ js/route.js
@@ -3,5 +3,7 @@ app.config(function($routeProvider) {
     $routeProvider
         .when("/", {
             templateUrl : "views/home.html"
+        }).when("/educacion", {
+            templateUrl : "views/educacion_view.html"
         });
 });
