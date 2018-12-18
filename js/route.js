5a38c7af1538c5af4a92d6271c5397748b699540
soycodigo <soycodigo@gmail.com>
2018-12-18 07:44:56 -0700

    Se hizo página de inicio y cambios en el contenido

--- js/route.js
+++ js/route.js
@@ -2,6 +2,9 @@ var app = angular.module("myApp", ["ngRoute"]);
 app.config(function($routeProvider) {
     $routeProvider
         .when("/", {
+            templateUrl : "views/front.html"
+        })
+        .when("/home", {
             templateUrl : "views/home.html"
         }).when("/educacion", {
             templateUrl : "views/educacion_view.html"
